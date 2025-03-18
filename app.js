1875c514e391f2643a85261d38894d3a0c8daa8f
djmm <djmeridama@outlook.com>
2025-03-17 19:23:57 -0600

    main

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El array que almacenara a los participantes en el concurso del amigo secreto 
+let amigos = []
