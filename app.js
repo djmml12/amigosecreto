462fb4f31717c9c0c08016e64a2993899d717e75
djmm <djmeridama@outlook.com>
2025-03-18 01:02:43 -0600

    funcion que crea el listado

--- app.js
+++ app.js
@@ -7,6 +7,30 @@ function agregarNombre() {
     if (nombre === "") {
         alert("Ingresa un nombre para continuar");
     } else{
-        nombres.push(nombre); //Agrega el nombre al Array
+        amigo.push(nombre); //Agrega el nombre al Array
     }
 }
+
+function crearLista() {
+    let listaAmigos = document.querySelector("#listaAmigos");
+  listaAmigos.innerHTML = "";
+
+  for (let index = 0; index < amigos.length; index++) {
+    const element = amigos[index];
+
+    let listaHTML = document.createElement("li");
+    listaHTML.textContent = element;
+    listaAmigos.appendChild(listaHTML);
+  }
+}
+
+function sortearAmigo() {
+    let cantidadAmigos = amigos.length;
+    if (cantidadAmigos === 0) {
+      alert("Por favor, inserte un nombre antes de sortear");
+    } else {
+      let indiceAmigo = Math.floor(Math.random() * cantidadAmigos);
+      let resultadoHTML = document.querySelector("#resultado");
+      resultadoHTML.innerHTML = amigos[indiceAmigo];
+    }
+  }
