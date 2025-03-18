b03c9bca96ef9dc77fb447deb7cca8103fa5a07f
djmm <djmeridama@outlook.com>
2025-03-17 22:23:53 -0600

    agrega nombres al array despues de verificar texto

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-// El array que almacenara a los participantes en el concurso del amigo secreto 
-let amigos = []
+let amigos = [] // El array que almacenara a los participantes en el concurso del amigo secreto 
+
+function agregarNombre() {
+    let input = document.getElementById("amigo")
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Ingresa un nombre para continuar");
+    } else{
+        nombres.push(nombre); //Agrega el nombre al Array
+    }
+}
